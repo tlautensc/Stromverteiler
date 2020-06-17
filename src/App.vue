a5ef3d9a5b26022864cb69cf914157eacce4f2a5
tlautensc <34093354+tlautensc@users.noreply.github.com>
2020-06-17 19:52:11 +0200

    first tidy up

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,32 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="page-container">
+      <md-app md-waterfall md-mode="overlap">
+        <md-app-toolbar class="md-primary md-large">
+          <div class="md-toolbar-row">
+            <md-button class="md-icon-button" @click="$router.back()" v-if="!(this.$route.name == 'Home') && !(this.$route.name == 'Login')">
+              <md-icon>arrow_back</md-icon>
+            </md-button>
+            
+            <span class="md-title" style="color: white">Buchrezept</span>
+            <md-button class="md-icon-button" style="position: fixed; right: 0" @click="reloadPage" >
+              <md-icon>refresh</md-icon>
+            </md-button>
+          </div>
+        </md-app-toolbar>
+
+        <md-app-content style="margin-bottom: 60px;">
+          <router-view/>
+        </md-app-content>
+      </md-app>
     </div>
-    <router-view/>
+    <div class="phone-viewport">
+      <md-bottom-bar md-sync-route md-mode="shift">
+        <md-bottom-bar-item style="max-width: none" to="/" exact md-icon="home"></md-bottom-bar-item>
+        <md-bottom-bar-item style="max-width: none" to="/add" md-icon="add"></md-bottom-bar-item>
+        <md-bottom-bar-item style="max-width: none" to="/settings" md-icon="settings"></md-bottom-bar-item>
+      </md-bottom-bar>
+    </div>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+import "./assets/materialKit.scss"
+
+Vue.use(VueMaterial)
 
 Vue.config.productionTip = false
 
