e5e28b900ef880467faee3407e4611525f509bc8
tlautensc <34093354+tlautensc@users.noreply.github.com>
2020-06-19 17:08:46 +0200

    vue iframe test

--- src/main.js
+++ src/main.js
@@ -7,8 +7,10 @@ import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
 import "./assets/materialKit.scss"
+import VueFriendlyIframe from 'vue-friendly-iframe'
 
 Vue.use(VueMaterial)
+Vue.use(VueFriendlyIframe)
 
 Vue.config.productionTip = false
 
