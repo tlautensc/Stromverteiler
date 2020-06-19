1cf240315109930cb86604aeea1db8598debf8a6
tlautensc <34093354+tlautensc@users.noreply.github.com>
2020-06-19 17:13:45 +0200

    Commit Conflict

--- src/views/Live.vue
+++ src/views/Live.vue
@@ -2,18 +2,9 @@
   <div class="home">
     <h1>Live</h1>
     <div class="md-layout md-gutter md-alignment-center">
-<<<<<<< HEAD
       <div class="md-layout-item md-xlarge-size-33 md-large-size-33 md-size-medium-33 md-small-size-50 md-xsmall-size-100">
             <iframe src="http://localhost:3000/d-solo/bOCUe4WGz/test-dashboard?orgId=1&from=1592555714590&to=1592577314591&panelId=2&refresh=1s" width="350" height="500" frameborder="0"></iframe>
       </div>
-=======
-      <div class="md-layout-item md-medium-size-33 md-small-size-50 md-xsmall-size-100"></div>
-      <div class="md-layout-item md-medium-size-33 md-small-size-50 md-xsmall-size-100"></div>
-      <div class="md-layout-item md-medium-size-33 md-small-size-50 md-xsmall-size-100"></div>
-      <div class="md-layout-item md-medium-size-33 md-small-size-50 md-xsmall-size-100"></div>
-      <div class="md-layout-item md-medium-size-33 md-small-size-50 md-xsmall-size-100"></div>
-      <div class="md-layout-item md-medium-size-33 md-small-size-50 md-xsmall-size-100"></div>
->>>>>>> master
     </div>
     
   </div>
@@ -30,23 +21,12 @@ export default {
 @import "~vue-material/dist/theme/engine"; // Import the theme engine 
 
   .md-layout-item {
-<<<<<<< HEAD
-=======
-    height: 200px;
->>>>>>> master
     margin-top: 8px;
     margin-bottom: 8px;
 
     &:after {
       width: 100%;
-<<<<<<< HEAD
       background: md-get-palette-color(red, 200);
-=======
-      height: 100%;
-      display: block;
-      background: md-get-palette-color(red, 200);
-      content: " ";
->>>>>>> master
     }
   }
 </style>
